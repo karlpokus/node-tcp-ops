16af110f784ffc905661145ef0a02c3da53d2e76
Karl Pokus <karl.pokus@gmail.com>
2018-02-25 13:29:19 +0100

    add router

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const net = require('net');
-const querystring = require('querystring');
-const url = require('url');
 
 const srv = http.createServer();
+const router = require('./lib/router');
+
 let people; // socket
 let pets;
 
@@ -36,32 +36,18 @@ const connectToPets = () => {
 function runQuery(service, cmd, payload, res) {
 	// req.pipe(people, {end: false}).once('data', res.end.bind(res));
 	service.write(JSON.stringify({ cmd, payload }));
-	service.once('data', res.end.bind(res));
+	service.once('data', res.end.bind(res)); // add res.writeHead(<num>) here
 }
 
-srv.on('request', (req, res) => {
-	const { pathname, query } = url.parse(req.url);
+router.add('GET', '/people', (req, res, data) => {
+	runQuery(people, 'getAll', null, res);
+});
 
-	if (req.method === 'GET' && pathname === '/people') {
-		runQuery(people, 'getAll', null, res);
+router.add('POST', '/people', (req, res, data) => {
+	runQuery(people, 'create', data, res);
+});
 
-	} else if (req.method === 'POST' && pathname === '/people') {
-		runQuery(people, 'create', querystring.parse(query), res);
-
-	} else if (req.method === 'GET' && pathname === '/pets') {
-			//callService(pets, 'getAll', null, res);
-
-	} else if (req.method === 'POST' && pathname === '/pets') {
-		//callService(pets, 'create', querystring.parse(query), res);
-
-	} else {
-		res.writeHead(404)
-		return res.end();
-	}
-})
-
-// start
-srv.listen(5001, () => {
+srv.on('request', router.go.bind(router)).listen(5001, () => {
 	connectToPeople();
 	//connectToPets();
 });
