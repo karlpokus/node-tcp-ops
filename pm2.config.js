4c13e35cec0939a2b62d14408dd7ff452772e514
Karl Pokus <karl.pokus@gmail.com>
2018-02-26 17:52:46 +0100

    add watcher and cluster mode all procs

--- pm2.config.js
+++ pm2.config.js
@@ -7,8 +7,10 @@ const procs = [
 const singleConfig = proc => ({
 	name: proc,
 	script: `./${ proc }.js`,
-	//watch: `./${ proc }.js`,
-	log_date_format: 'YYYY-MM-DD HH:mm Z'
+	watch: `./${ proc }.js`,
+	log_date_format: 'YYYY-MM-DD HH:mm Z',
+	instances: 2,
+	exec_mode: 'cluster'
 });
 
 module.exports = {
